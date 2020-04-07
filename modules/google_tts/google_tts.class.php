8b94390d7d81ac7c6023f7a16fd5a07941f520cf
tarasfrompir <tarasfrompir@inbox.ru>
2020-04-07 21:28:18 +0300

    Update google_tts.class.php

--- modules/google_tts/google_tts.class.php
+++ modules/google_tts/google_tts.class.php
@@ -20,7 +20,7 @@ class google_tts extends module {
      * @access private
      */
     function google_tts() {
-        $this->name = "google_tts";
+        $this->name = "google_tts_seample";
         $this->title = "Google TTS";
         $this->module_category = "<#LANG_SECTION_APPLICATIONS#>";
         $this->checkInstalled();
